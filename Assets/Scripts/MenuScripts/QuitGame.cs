d8979f0fb73d4eb41dfebd45684860cfef25dda9
mashwin1993 <31690715+mashwin1993@users.noreply.github.com>
2017-09-11 20:14:56 +1000

    Adding a Main Menu

---
+++ Assets/Scripts/MenuScripts/QuitGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        // If in unity
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        Application.Quit();
+    }
+}
