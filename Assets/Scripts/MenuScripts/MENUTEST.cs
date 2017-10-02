e5dcb37215f7f07f93065c1af9c6a673ccd1ae5a
Nathan <nathanjbrown@live.com>
2017-10-02 12:54:35 +1000

    small update to test level loader script

--- Assets/Scripts/MenuScripts/MENUTEST.cs
+++ Assets/Scripts/MenuScripts/MENUTEST.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MENUTEST : MonoBehaviour {
 
-	public int[] LevelParts;
+	public string[] LevelParts;
 	public GUIStyle buttonstyle;
 	public GUIStyle labelstyle;
 	public Transform LevelContinue;
@@ -22,8 +22,16 @@ public class MENUTEST : MonoBehaviour {
 	
 	void OnGUI() {
 		buttonpos.y = 20;
+		if (GUI.Button(new Rect(buttonpos.x-(buttonsize.x*0.5f), buttonpos.y, buttonsize.x*2, buttonsize.y), "Reset Save", buttonstyle)){
+			PlayerPrefs.SetInt("PlayersChapter", 0);
+			PlayerPrefs.SetInt("PlayersLevel", 0);
+			savedChapter = 0;
+			savedLevel = 0;
+		}
+		buttonpos.y += buttonsize.y + 20;
 		for (int i = 1; i <= LevelParts.Length; i++){
-			for (int j = 1; j <= LevelParts[i-1]; j++){
+			Object[] textures = Resources.LoadAll("Levels/"+LevelParts[i-1], typeof(Texture2D));
+			for (int j = 1; j <= textures.Length; j++){
 				if (savedChapter >= i && savedLevel >= j){
 					if (GUI.Button(new Rect(buttonpos.x, buttonpos.y, buttonsize.x, buttonsize.y), i + "-" + j, buttonstyle)){
 						Transform levelloader = Instantiate(LevelContinue, new Vector3(-99, -99, 0), transform.rotation);
