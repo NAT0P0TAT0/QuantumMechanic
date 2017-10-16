ac04e8a8cda195a8ebc99012125720090ea8b6cf
mashwin1993 <31690715+mashwin1993@users.noreply.github.com>
2017-10-16 22:25:54 +1000

    Main Menu and Pause Menus implementation

---
+++ Assets/Scripts/MenuScripts/PauseGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour {
+
+
+    //publics
+    public GameObject PausePanel;
+
+    //Methods
+    //Restart
+    public void Restart()
+    {
+        Time.timeScale = 1;
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Time.timeScale = 0;
+            PausePanel.SetActive(true);
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/MenuScripts/ToggleUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleUpdate : MonoBehaviour {
+
+
+    //
+    public Toggle Toggle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        Screen.fullScreen = Toggle.isOn;
+        Debug.Log(Screen.fullScreen);
+
+    }
+}
